83fbd4498df51f67636f46183c60611544d7d684
Raunaq Naidu <raunaq.naidu@gmail.com>
2016-02-09 20:33:36 -0800

    bugfix for DELETE and removing some debug spew

--- server.js
+++ server.js
@@ -39,7 +39,6 @@ app.post('/todos',function (req,res){
 
 	var body = _.pick(req.body,'description','completed');
 	var temp =body.completed;
-	console.log(_.isString(body.description));
 	if (!(body.completed === 'true' || body.completed === 'false') || !_.isString(body.description) || body.description.trim().length === 0){
 		return res.status(400).send();
 	}
@@ -55,14 +54,15 @@ app.delete('/todos/:id',function (req, res){
 	var matchedTodo;
 	var todoId = parseInt(req.params.id,10);
 	matchedTodo = _.findWhere(todos,{id: todoId});
-
 	if (matchedTodo){
-		todos = _.without(todos,{id : todoId});
+		todos = _.without(todos,matchedTodo);
 		res.json(matchedTodo);
 	}else{
 		res.status(404).json({"error": "no todo found with this id"});
 	}
 });
+
+
 app.listen(PORT, function (){
 	console.log('Starting raunaq\'s server on port ' + PORT);
 });
