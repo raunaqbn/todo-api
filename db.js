8ed6e5368815ffa4b191cfd2e925ef7671f90c36
Raunaq Naidu <raunaq.naidu@gmail.com>
2016-02-15 12:23:55 -0800

    Added user model and post method to create user

--- db.js
+++ db.js
@@ -10,13 +10,15 @@ if (env === 'production'){
      sequelize = new Sequelize(undefined, undefined, undefined,{
 	'dialect' : 'sqlite',
 	'storage' : __dirname + '/data/dev-todo-api.sqlite'
-});
+	});
 }
 
 var db = {};
 
-db.todo = sequelize.import(__dirname + '/models/todo.js')
+db.todo = sequelize.import(__dirname + '/models/todo.js');
+db.user = sequelize.import(__dirname + '/models/user.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
 
 module.exports = db;
--- server.js
+++ server.js
@@ -123,6 +123,16 @@ app.delete('/todos/:id', function(req, res) {
 });
 
 
+app.post('/users',function(req ,res){
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function(user){
+		res.json(user.toJSON());
+	},function(e){
+		res.status(400).json(e);
+	});
+});
+
 db.sequelize.sync().then(function () {
 	app.listen(PORT, function() {
 	console.log('Starting raunaq\'s server on port ' + PORT);
